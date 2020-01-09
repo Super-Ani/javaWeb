6bcaf51e33249ac51701a55652f07973ea125a18
nizhou <57421522@qq.com>
2020-01-09 22:24:20 +0800

    哈哈哈哈

--- src/com/nizhou/servuceImpl/userSrvImpl.java
+++ src/com/nizhou/servuceImpl/userSrvImpl.java
@@ -1,4 +1,18 @@
 package com.nizhou.servuceImpl;
 
-public class userSrvImpl {
+import com.nizhou.DAO.userDao;
+import com.nizhou.DTO.userDto;
+import com.nizhou.service.userSrv;
+
+import javax.annotation.Resource;
+
+public class userSrvImpl implements userSrv {
+    @Resource
+    private userDao userDao;
+
+    @Override
+    public userDto selectUserInfo(String username){
+        userDto userDto=userDao.selectUesrInfo(username);
+        return userDto;
+    }
 }
